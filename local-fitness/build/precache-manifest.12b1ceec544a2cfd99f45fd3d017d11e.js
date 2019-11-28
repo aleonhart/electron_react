710fa31ae74dc85ee9e158215b4aeba8e6b89533
Alex Leonhart <alexleonhartcode@gmail.com>
2019-11-27 22:01:17 -0800

    finally have Electron + ReactJS + ChartJS working together

---
+++ local-fitness/build/precache-manifest.12b1ceec544a2cfd99f45fd3d017d11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f135429532a363bbb9b5b7e512cc42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8110a848eeafb3ec92fa",
+    "url": "./static/css/main.6f790844.chunk.css"
+  },
+  {
+    "revision": "fadefa116320c63a1dfb",
+    "url": "./static/js/2.5e84040d.chunk.js"
+  },
+  {
+    "revision": "8110a848eeafb3ec92fa",
+    "url": "./static/js/main.11ed5588.chunk.js"
+  },
+  {
+    "revision": "1d95a232cc8670168cd1",
+    "url": "./static/js/runtime-main.831ab146.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
